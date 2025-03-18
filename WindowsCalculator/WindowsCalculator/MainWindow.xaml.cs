ece0efbbc2bad1985f4236f59292a3e8fb61d59f
Vasile Rares <raresmihail2004@gmail.com>
2025-03-18 22:38:49 +0200

    Updated UI (hamburger and SideMenu)

--- WindowsCalculator/WindowsCalculator/MainWindow.xaml.cs
+++ WindowsCalculator/WindowsCalculator/MainWindow.xaml.cs
@@ -1,10 +1,12 @@
-﻿using System.Text;
+﻿using System;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -17,6 +19,8 @@ namespace WindowsCalculator;
 /// </summary>
 public partial class MainWindow : Window
 {
+    private bool isMenuOpen = false;
+
     public MainWindow()
     {
         InitializeComponent();
@@ -42,5 +46,52 @@ public partial class MainWindow : Window
     private void CloseButton_Click(object sender, RoutedEventArgs e)
     {
         Close();
+    }
+
+    // Eveniment pentru butonul hamburger
+    private void HamburgerButton_Click(object sender, RoutedEventArgs e)
+    {
+        if (!isMenuOpen)
+            OpenMenu();
+        else
+            CloseMenu();
+    }
+
+    // Eveniment pentru închiderea meniului când se apasă pe overlay
+    private void CloseMenu_Click(object sender, MouseButtonEventArgs e)
+    {
+        CloseMenu();
+    }
+
+    // Deschide meniul lateral cu animație
+    private void OpenMenu()
+    {
+        // Creează animația pentru meniu
+        DoubleAnimation menuAnimation = new DoubleAnimation
+        {
+            From = 0,
+            To = 205, // Lățimea meniului
+            Duration = TimeSpan.FromSeconds(0.3)
+        };
+
+        SideMenuTransform.BeginAnimation(TranslateTransform.XProperty, menuAnimation);
+
+        isMenuOpen = true;
+    }
+
+    // Închide meniul lateral cu animație
+    private void CloseMenu()
+    {
+        // Creează animația pentru meniu
+        DoubleAnimation menuAnimation = new DoubleAnimation
+        {
+            From = 205, // Lățimea meniului
+            To = 0,
+            Duration = TimeSpan.FromSeconds(0.3)
+        };
+
+        SideMenuTransform.BeginAnimation(TranslateTransform.XProperty, menuAnimation);
+
+        isMenuOpen = false;
     }
 }
